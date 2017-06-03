c91a3177c458b7e1679cbfe54b2c558cdb806527
James Silberbauer <jamessil@telkomsa.net>
2017-06-03 08:56:30 +0200

    Query for building menu.

--- repositories/program_function_repo.rb
+++ repositories/program_function_repo.rb
@@ -2,4 +2,25 @@ require 'rom-repository'
 
 class ProgramFunctionRepo < ROM::Repository[:program_functions]
   commands :create
+
+  def menu_for_user(user)
+    query = <<-EOQ
+    SELECT f.id AS functional_area_id, p.id AS program_id, pf.id,
+    f.functional_area_name, p.program_name, pf.group_name, pf.program_function_name,
+    pf.url, pf.program_function_sequence
+    FROM program_functions pf
+    JOIN programs p ON p.id = pf.program_id
+    JOIN program_users pu ON pu.program_id = pf.program_id
+    JOIN functional_areas f ON f.id = p.functional_area_id
+    WHERE pu.user_id = #{user.id}
+      AND (NOT pf.restricted_user_access OR EXISTS(SELECT id FROM program_function_users
+      WHERE program_function_id = pf.id
+        AND user_id = #{user.id}))
+        AND f.active
+        AND p.active
+        AND pf.active
+    ORDER BY f.functional_area_name, p.program_name, pf.group_name, pf.program_function_sequence
+    EOQ
+    DB.base[query].all
+  end
 end
